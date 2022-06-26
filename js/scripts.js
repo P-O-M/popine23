31d85524baaebd17b2a244f33be1f1a41f345daa
POM <po.martin.engineer@gmail.com>
2022-06-26 16:37:35 +0200

    maps api key

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 44.838651, lng: -0.216690};
+    var location = {lat: 44.838652, lng: -0.216652};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 44.838651, lng: -0.216690};
+    var la_fiesta = {lat: 44.838652, lng: -0.216652};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
